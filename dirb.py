8bbbc84bbb4b4b5323fa64064cec0dab44e6bbe9
liixhunter <123924710+liixhunter@users.noreply.github.com>
2023-03-06 01:37:44 +0300

    Add files via upload

---
+++ dirb.py
@@ -0,0 +1,54 @@
+import threading
+import requests
+import queue
+import time
+import sys
+import keyboard  # pip install keyboard
+
+
+class BruteForcer:
+    def __init__(self, wordlist_file, target_url, threads=10):
+        self.target_url = target_url
+        self.threads = threads
+        self.word_queue = queue.Queue()
+        self.load_wordlist(wordlist_file)
+
+    def load_wordlist(self, wordlist_file):
+        with open(wordlist_file, "r") as f:
+            for line in f:
+                word = line.strip()
+                if word:
+                    self.word_queue.put(word)
+
+    def brute_force(self):
+        while not self.word_queue.empty():
+            if keyboard.is_pressed("esc"):
+                print("User stopped the process.")
+                return
+            word = self.word_queue.get()
+            url = self.target_url + "/" + word
+            try:
+                response = requests.get(url)
+                if response.status_code == 200:
+                    print(f"[+] Found: {url}")
+            except requests.exceptions.RequestException:
+                pass
+
+    def run(self):
+        threads = []
+        for i in range(self.threads):
+            thread = threading.Thread(target=self.brute_force)
+            thread.start()
+            threads.append(thread)
+
+        for thread in threads:
+            thread.join()
+
+
+if __name__ == "__main__":
+    wordlist_file = "directories.txt"
+    target_url = "https://mayexperinfo.com/"
+    threads = 20
+
+    bf = BruteForcer(wordlist_file, target_url, threads)
+    bf.run()
